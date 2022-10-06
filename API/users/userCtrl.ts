44e076bddad7772c0670db5a447b6cda64ba310d
lioGR <liorgrishinnew@gmail.com>
2022-10-06 12:37:53 +0300

    Update userCtrl.ts

--- API/users/userCtrl.ts
+++ API/users/userCtrl.ts
@@ -1,15 +1,14 @@
 import express from "express";
 
-import { UserModel } from "./userModel";
-import { UserValidation } from "./userModel";
+import { UserModel, UserValidation } from "./userModel";
 
 export async function register(req: express.Request, res: express.Response) {
     try {
         const { email, username, password, confirmPassword } = req.body;
         if (!email || !username || !password || !confirmPassword) throw new Error("Missing fields in register");
 
-        const { error } = UserValidation.validate({email, username, password, confirmPassword});
-        if(error) throw error;
+        const { error } = UserValidation.validate({ email, username, password, confirmPassword });
+        if (error) throw error;
 
         const userDB = await new UserModel({ email, password, username });
         await userDB.save();
@@ -19,8 +18,6 @@ export async function register(req: express.Request, res: express.Response) {
         res.send({ error: error.message });
     }
 }
-
-
 
 export async function login(req: express.Request, res: express.Response) {
     try {
@@ -33,5 +30,19 @@ export async function login(req: express.Request, res: express.Response) {
         res.send({ login: true, userDB });
     } catch (error) {
         res.send({ error: error.message });
+    }
+}
+
+export async function getUser(req: express.Request, res: express.Response) {
+    try {
+        const { userId } = req.body;
+        if(!userId) throw new Error("Couldn't receive userId from req.body");
+
+        const userDB = await UserModel.findById(userId);
+        if(!userDB) throw new Error(`Couldn't find user with id: ${userId}`);
+
+        res.send({userDB});
+    } catch (error) {
+        res.send({ error: error.message })
     }
 }
