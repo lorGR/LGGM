ab26e9496d62eaee8b10692393cfcb91449be651
lioGR <liorgrishinnew@gmail.com>
2022-10-07 11:37:36 +0300

    WIP

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import jwtDecode from 'jwt-decode';
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+    /* global google */
+    //@ts-ignore
+    google.accounts.id.initialize({
+      client_id: "924677506476-h5paqp1lqhvgnctprtq051dckuq2vvt1.apps.googleusercontent.com",
+      callback: handleGoogleLogin
+    })
+
+    //@ts-ignore
+    google.accounts.id.renderButton(
+      document.getElementById("signInDiv"),
+      {theme: "outline", size: "large"}
+    )
+  }, []);
+
+  const handleGoogleLogin = (response: any) => {
+    try {
+      const userData = jwtDecode(response.credential);
+      console.log(userData);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="signInDiv"></div>
     </div>
   );
 }
---
+++ client/src/features/google-login/GoogleLogin.tsx
@@ -0,0 +1,13 @@
+import { useEffect } from "react"
+
+const GoogleLogin = () => {
+    useEffect(() => {
+
+    }, []);
+
+    return (
+        <div>GoogleLogin</div>
+    )
+}
+
+export default GoogleLogin
