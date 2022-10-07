8351e95ef051e83c32fc5dbb556f762ced2fd30c
Gili <gilimena95@gmail.com>
2022-10-07 11:48:20 +0300

    WIP

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,36 +1,12 @@
-import React, { useEffect } from 'react';
-import jwtDecode from 'jwt-decode';
 import './App.css';
+import GoogleLogin from './features/google-login/GoogleLogin';
 
 function App() {
 
-  useEffect(() => {
-    /* global google */
-    //@ts-ignore
-    google.accounts.id.initialize({
-      client_id: "924677506476-h5paqp1lqhvgnctprtq051dckuq2vvt1.apps.googleusercontent.com",
-      callback: handleGoogleLogin
-    })
-
-    //@ts-ignore
-    google.accounts.id.renderButton(
-      document.getElementById("signInDiv"),
-      {theme: "outline", size: "large"}
-    )
-  }, []);
-
-  const handleGoogleLogin = (response: any) => {
-    try {
-      const userData = jwtDecode(response.credential);
-      console.log(userData);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
   return (
     <div className="App">
-      <div id="signInDiv"></div>
+      {/* <div id="signInDiv"></div> */}
+      <GoogleLogin />
     </div>
   );
 }
--- client/src/features/google-login/GoogleLogin.tsx
+++ client/src/features/google-login/GoogleLogin.tsx
@@ -1,12 +1,32 @@
 import { useEffect } from "react"
+import jwtDecode from 'jwt-decode';
 
 const GoogleLogin = () => {
     useEffect(() => {
-
+        /* global google */
+        //@ts-ignore
+        google.accounts.id.initialize({
+            client_id: "924677506476-h5paqp1lqhvgnctprtq051dckuq2vvt1.apps.googleusercontent.com",
+            callback: handleGoogleLogin
+        })
+        //@ts-ignore
+        google.accounts.id.renderButton(
+            document.getElementById("googleLogin"),
+            {theme: "outline", size: "large"}
+        )
     }, []);
 
+    const handleGoogleLogin = (response: any) => {
+        try {
+            const userData = jwtDecode(response.credential);
+            console.log(userData)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
     return (
-        <div>GoogleLogin</div>
+        <div id="googleLogin"></div>
     )
 }
 
